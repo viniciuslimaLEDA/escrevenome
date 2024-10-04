8603ddc210cf5558fa87887dea94bd36d7dc84ae
viniciuslimaLEDA <00001115234262sp@al.educacao.sp.gov.br>
2024-10-04 18:52:54 -0300

    alterando canvas

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let posicaoHorizontal; // x
 let posicaoVertical; // y
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
    background(color(100,0,0));
   cor = color(random(0,255), random(0,255), random(0,255));
   posicaoHorizontal = 200;
